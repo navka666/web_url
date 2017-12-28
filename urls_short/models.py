4ddc397bd76ef179c00a0a4d4804e4ef377319d0
navka666 <navka6677@gmail.com>
2017-12-28 19:33:15 +0200

    Update model url

--- urls_short/models.py
+++ urls_short/models.py
@@ -1,7 +1,9 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 class URL(models.Model):
     url_max = models.URLField(max_length=80, null=True)
     click = models.PositiveIntegerField(default=0)
     url_short = models.CharField(blank=True, max_length=30, unique=True, null=True)
     date_of_create = models.DateTimeField(auto_now_add=True)
+    user  = models.ForeignKey(User, null=True, blank=True, related_name='url_user')
